b5340f8766c375fcf2c83770a17a992f96945a9b
Andrew Parker <aparker42@gmail.com>
2017-04-16 22:53:43 +0100

    Add support for on-disk storage
    This is a very, very basic on-disk storage mechanism. It keeps the events
    in a data file that is append only and keeps and "index" of the IDs and
    positions in an index file. The index file is really no better than just
    scanning the data file.

--- src/org/bgprocess/eventual/DiskStorage.kt
+++ src/org/bgprocess/eventual/DiskStorage.kt
@@ -1,24 +1,103 @@
 package org.bgprocess.eventual
 
+import java.io.Closeable
+import java.nio.ByteBuffer
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+import java.nio.file.OpenOption
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import java.time.Instant
 import java.util.*
+import kotlin.collections.HashMap
 
-class DiskStorage : Storage {
+class DiskStorage(val dataDir: Path) : Storage {
+    val streams = HashMap<String, DiskStream>()
+
     override fun stream(streamName: String): Stream {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return streams.getOrPut(streamName, {
+            DiskStream(dataDir.resolve(streamName))
+        })
     }
 }
 
-class DiskStream : Stream {
+/**
+ * Data file format
+ *
+ * Version | Event Data Entries: UUID | Write Time (Millis since Epoch) | Data Size | Data      |
+ * 1 byte  | 16 bytes                 | 8 bytes                         | 2 bytes   | data size |
+ *
+ */
+class DiskStream(val dataFile: Path) : Stream, Closeable {
+    val HEAD_ID = UUID(0, 0)
+    val file = FileChannel.open(dataFile, StandardOpenOption.CREATE, StandardOpenOption.READ, StandardOpenOption.WRITE)
+    val index = FileChannel.open(dataFile.parent.resolve(dataFile.fileName.toString() + ".idx"), StandardOpenOption.CREATE, StandardOpenOption.READ, StandardOpenOption.WRITE)
+    var currentHeadId: UUID = HEAD_ID
+
     override fun read(startId: UUID, length: Int, function: (Event) -> Unit) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val buffer = ByteBuffer.allocate(20)
+        if(startId != HEAD_ID) {
+            for (location in 0..index.size() step 20) {
+                index.read(buffer, location)
+                // Don't allocate a UUID object for this check. No need for more garbage
+                if ((buffer.getLong(0) == startId.mostSignificantBits) and (buffer.getLong(8) == startId.leastSignificantBits)) {
+                    playEvents(buffer.getInt(16), function)
+                }
+            }
+        } else {
+            playEvents(0, function)
+        }
+    }
+
+    private fun playEvents(position: Int, function: (Event) -> Unit) {
+        var currentPosition = position
+        val map = file.map(FileChannel.MapMode.READ_ONLY, position.toLong(), file.size() - position)
+
+        while (currentPosition < file.size()) {
+            val size = map.getShort(currentPosition + 24)
+            val data = ByteArray(size.toInt())
+            map.position(currentPosition + 26)
+            map.get(data)
+            function(Event(UUID(map.getLong(currentPosition), map.getLong(currentPosition + 8)), data))
+            currentPosition += 26 + size
+        }
     }
 
     override fun write(headId: UUID, id: UUID, data: ByteArray): WriteResult {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if(currentHeadId != headId) {
+            return WriteResult(false, "Head not at expected event")
+        }
+
+        return write(id, data)
     }
 
     override fun write(id: UUID, data: ByteArray): WriteResult {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val position = file.size()
+        file.position(position)
+        val dataBuffer = ByteBuffer.allocate(16 + 8 + 2 + data.size)
+        dataBuffer.putLong(id.mostSignificantBits)
+                .putLong(id.leastSignificantBits)
+                .putLong(Instant.now().toEpochMilli())
+                .putShort(data.size.toShort())
+                .put(data)
+                .rewind()
+        file.write(dataBuffer)
+        file.force(false)
+
+        val indexBuffer = ByteBuffer.allocate(16 + 4)
+        indexBuffer.putLong(id.mostSignificantBits)
+                .putLong(id.leastSignificantBits)
+                .putInt(position.toInt())
+                .rewind()
+        index.write(indexBuffer, index.size())
+
+        currentHeadId = id
+
+        return WriteResult(true, "Wrote ${id}")
+    }
+
+    override fun close() {
+        file.close()
     }
 }
 
--- src/org/bgprocess/eventual/Main.kt
+++ src/org/bgprocess/eventual/Main.kt
@@ -2,6 +2,9 @@ package org.bgprocess.eventual
 
 import java.net.InetAddress
 import java.net.InetSocketAddress
+import java.nio.file.FileSystem
+import java.nio.file.FileSystems
+import java.nio.file.Path
 import java.util.logging.LogManager
 import java.util.logging.Logger
 import java.util.logging.SimpleFormatter
@@ -9,7 +12,9 @@ import java.util.logging.StreamHandler
 
 fun main(args: Array<String>) {
     Logger.getGlobal().addHandler(StreamHandler(System.out, SimpleFormatter()))
-    val server = Server(InetSocketAddress(InetAddress.getLocalHost(), 4455), MemoryStorage())
+    //val storage = MemoryStorage()
+    val storage = DiskStorage(FileSystems.getDefault().getPath("."))
+    val server = Server(InetSocketAddress(InetAddress.getLocalHost(), 4455), storage)
 
     server.start()
 }
